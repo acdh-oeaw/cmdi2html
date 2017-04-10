2bab41de733614054fda064b33704e858ba80709
Asil Çetin <a1100130@unet.univie.ac.at>
2017-04-10 11:00:10 +0200

    error page for missing or invalid cmdi

---
+++ error_page.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8"/>
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	    <!-- The above 3 meta tags *must* come first in the head -->   
+	    <meta name="description" content=""/>
+	    <meta name="author" content=""/>
+	    <link rel="icon" href="favicon.ico"/>
+	    <title>
+	        CMDI2HTML by ÖAW-ACDH
+	    </title>
+	    <!-- Bootstrap core CSS -->
+	    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	    <link rel="stylesheet" type="text/css" href="css/ie10-viewport-bug-workaround.css"/>
+	    <!-- Custom styles for this template -->
+	    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+	<body>
+	    <div class="container">
+	      	<div class="header clearfix">
+				<h3 class="service-name">CMDI2HTML</h3>
+				<a class="navbar-brand" target="_blank" href="http://www.oeaw.ac.at/acdh/en"><img src="img/acdhlogo.png"/></a>
+	    	</div>				
+			<div class="jumbotron">
+	        	<h2>Error</h2>
+				<span>
+					<strong>Sorry, either your cmdi parameter is empty or not a valid file.</strong>
+				</span>			
+	      	</div>
+      	</div> <!-- /container -->
+	</body>
+</html>
