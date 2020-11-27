65dc71589490485bc4c683cdfc3833154f26e194
Mateusz <mzoltak@oeaw.ac.at>
2020-11-27 22:01:41 +0100

    Uncommited changes commited

--- error_page.php
+++ error_page.php
@@ -7,7 +7,12 @@
 	    <!-- The above 3 meta tags *must* come first in the head -->   
 	    <meta name="description" content=""/>
 	    <meta name="author" content=""/>
-	    <link rel="icon" href="favicon.ico"/>
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>				
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>				
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>				
+		<link rel="manifest" href="/manifest.json"/>				
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00aba9"/>				
+		<meta name="theme-color" content="#ffffff"/>
 	    <title>
 	        CMDI2HTML by ÖAW-ACDH
 	    </title>
